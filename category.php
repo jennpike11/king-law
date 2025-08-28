353eb271d4a71849c703f3c6529b2d409bd8a73b
Jennifer Pike <jpike@csis.org>
2025-08-28 13:51:56 -0700

    single post

--- category.php
+++ category.php
@@ -15,9 +15,9 @@ get_header(); ?>
           <h2 class="category-header__heading"><?php single_cat_title('', true); ?></h2>
           <div class="category-header__subheading"><?php echo category_description(); ?></div>
         </div>
-      </div>
-      <div class="category-content__search-form">
-        <?php get_search_form(); ?>
+        <div class="category-content__search-form">
+          <?php get_search_form(); ?>
+        </div>
       </div>
     </div>
 
@@ -25,7 +25,7 @@ get_header(); ?>
       <?php if (have_posts()) {
         // The Loop
         while (have_posts()) : the_post(); ?>
-          <div class="category-content__item">
+          <a class="category-content__item" href="<?php the_permalink(); ?>">
             <?php
             // Use a safe builtin thumbnail call instead of a custom helper
             if (has_post_thumbnail()) {
@@ -37,19 +37,19 @@ get_header(); ?>
               ]);
             }
             ?>
-            <a class="category-content__content" href="<?php the_permalink(); ?>">
+            <div class="category-content__content">
               <h3 class="category-content__title"><?php the_title(); ?></h3>
               <div class="category-content__date"><?php the_time('F jS, Y'); ?></div>
               <div class="category-content__link">Read more</div>
-            </a>
-          </div>
+            </div>
+          </a>
         <?php endwhile;
       } else { ?>
         <p>Sorry, no posts matched your criteria.</p>
       <?php } ?>
     </div>
 
-    <a class="load-more load-more--category-posts">Load More</a>
+    <a class="load-more primary-button">Load More</a>
   </div>
 </section>
 
--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -9,12 +9,14 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="content-post">
-		<div class="content-post__image">
-			<?php echo get_the_post_thumbnail(); ?>
-		</div>	
-		<div class="content-post__heading">
-			<h1><?php the_title(); ?></h1>
-			<div class="content-post__date"><?php echo get_the_date(); ?></div>
+		<div class="content-post__header">
+			<div class="content-post__image">
+				<?php echo get_the_post_thumbnail(); ?>
+			</div>	
+			<div class="content-post__title">
+				<h1><?php the_title(); ?></h1>
+				<div class="content-post__date"><?php echo get_the_date(); ?></div>
+			</div>
 		</div>
 		<div class="content-post__content">
 			<?php the_content(); ?>
