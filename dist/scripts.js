b8a75e562b070c2311a9dd0433a98166e07e94a4
Jennifer Pike <jpike@csis.org>
2025-08-28 18:37:50 -0700

    commit

--- dist/scripts.js
+++ dist/scripts.js
@@ -450,7 +450,12 @@ if ($reviews.length && !$reviews.hasClass('slick-initialized')) {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    cssEase: 'linear'
+    cssEase: 'linear',
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 1 } },
+      { breakpoint: 600,  settings: { slidesToShow: 1, slidesToScroll: 1 } },
+      { breakpoint: 480,  settings: { slidesToShow: 1, slidesToScroll: 1 } }
+    ]
   });
 }
 
