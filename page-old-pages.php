8cebf5f1171a18999154704d7f7a11fd64ceda3e
Jennifer Pike <jpike@csis.org>
2025-09-02 13:51:58 -0700

    Added a template to accommodate old pages from the old website

--- page-old-pages.php
+++ page-old-pages.php
@@ -1,44 +1,67 @@
 <?php
-/**[
+/**
  * Template Name: Old Pages
  * Template Post Type: page
+ *
+ * @package prc-theme
  */
 
 get_header();
-?>  
+?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
+  <?php while ( have_posts() ) : the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <section class="hero-block__wrapper">
+        <?php
+        if ( has_post_thumbnail() ) {
+          the_post_thumbnail( 'full', array( 'class' => 'hero-block__image' ) );
+        }
+        ?>
+        <div class="hero-block">
+          <div class="hero-block__text">
+            <h1 class="hero-block__heading">
+              <?php echo wp_kses_post( get_the_title() ); ?>
+            </h1>
+          </div>
+          <div class="hero-block__king">
+            <img
+              src="<?php echo esc_url( 'https://kinglawstaging.wpenginepowered.com/wp-content/uploads/2025/08/arms_crossed_half_body.png' ); ?>"
+              alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+          </div>
+        </div>
+      </section>
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'prc-theme' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+      <div class="entry-content">
+        <?php
+        the_content(
+          sprintf(
+            wp_kses(
+              /* translators: %s: Post title for screen readers. */
+              __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'prc-theme' ),
+              array(
+                'span' => array(
+                  'class' => array(),
+                ),
+              )
+            ),
+            get_the_title()
+          )
+        );
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'prc-theme' ),
-				'after'  => '</div>',
-			)
-		); ?>
-		
-	</div><!-- .entry-content -->
+        wp_link_pages(
+          array(
+            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'prc-theme' ),
+            'after'  => '</div>',
+          )
+        );
+        ?>
+      </div><!-- .entry-content -->
 
-</article><!-- #post-<?php the_ID(); ?> -->
+    </article><!-- #post-<?php the_ID(); ?> -->
+  <?php endwhile; ?>
+</main><!-- #primary -->
 
-	</main><!-- #main -->
-
-<?php get_footer();
+<?php
+get_footer();
